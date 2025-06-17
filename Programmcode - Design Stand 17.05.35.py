3d9929734a52950647769010741ca5bcb6d4242d
vollmer03 <moritz.vollmer@bayern-mail.de>
2025-06-17 18:13:22 +0200

    Update Programmcode - Design Stand 17.05.25.py

--- Programmcode - Design Stand 17.05.35.py
+++ Programmcode - Design Stand 17.05.35.py
@@ -87,7 +87,7 @@ root = ctk.CTk()
 root.title("Mitarbeiterumfrage")
 root.geometry("1200x800")
 
-bg_image = Image.open("Ablage in lokaler Datei.png")
+bg_image = Image.open("Version 1.png")
 bg_photo = ImageTk.PhotoImage(bg_image)
 
 background_label = ctk.CTkLabel(root, image=bg_photo, text="", fg_color="transparent")
